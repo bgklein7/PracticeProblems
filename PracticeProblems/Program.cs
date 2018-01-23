a02cc13d336ad9e261565bffd61efac3c9d19656
bgklein7 <bgklein@outlook.com>
2018-01-22 19:37:32 -0500

    initial commit

---
+++ PracticeProblems/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string fullName = "Benjamin Gibson Klein";
+            //string favoriteCar = "Aston Martin DBS V12";
+            //string favoriteComicCharacter = "Harvey Pekar";
+            //Console.WriteLine("My full name is " + fullName);
+            //Console.WriteLine("My favorite car is " + favoriteCar);
+            //Console.WriteLine("My favorite comic character is " + favoriteComicCharacter);
+
+            //Lisa's muffins. unitOne represents total cups of sugar in recipe. unitTwo represents cups of sugar already added.
+            int unitOne = 7;
+            int unitTwo = 2;
+            Console.WriteLine(unitOne - unitTwo);
+
+            //Mike and Friends Tab.  unitOne represents number of payees. unitTwo represents amount each paid.
+            unitOne = 4;
+            unitTwo = 13;
+            Console.WriteLine(unitOne * unitTwo);
+
+            //Diapers. unitOne represents total money have to spend. unitTwo represents price per package.
+            unitOne = 40;
+            unitTwo = 8;
+            Console.WriteLine(unitOne / unitTwo);
+
+            //Trevon's wages. unitOne represents money he started with. unitTwo represents total money he ended up with.
+            unitOne = 29;
+            unitTwo = 41;
+            Console.WriteLine(unitTwo - unitOne);
+
+            //Pranav's running. unitOne represents Julie's miles. unitTwo represents the delta.
+            unitOne = 47;
+            unitTwo = 30;
+            Console.WriteLine(unitOne - unitTwo);
+
+            //Envelopes. unitOne represents total money have to spend. unitTwo represents price per envelope.
+            unitOne = 12;
+            unitTwo = 3;
+            Console.WriteLine(unitOne / unitTwo);
+
+            //Norachai's salad. unitOne represents cost of salad. unitTwo represents cash after purchase.
+            float unitThree = 5.12f;
+            float unitFour = 27.10f;
+            Console.WriteLine(unitThree + unitFour);
+
+
+            //double valueOne = 7;
+            //double valueTwo = 2;
+            //Console.WriteLine(valueOne - valueTwo);
+            //valueOne = 13;
+            //valueTwo = 4;
+            //Console.WriteLine(valueOne * valueTwo);
+            //valueOne = 40;
+            //valueTwo = 8;
+            //Console.WriteLine(valueOne / valueTwo);
+            //valueOne = 41;
+            //valueTwo = 29;
+            //Console.WriteLine(valueOne - valueTwo);
+            //valueOne = 47;
+            //valueTwo = 30;
+            //Console.WriteLine(valueOne - valueTwo);
+            //valueOne = 12;
+            //valueTwo = 3;
+            //Console.WriteLine(valueOne / valueTwo);
+            //valueOne = 27.10;
+            //valueTwo = 5.12;
+            //Console.WriteLine(valueOne + valueTwo);
+
+
+        }
+    }
+}
