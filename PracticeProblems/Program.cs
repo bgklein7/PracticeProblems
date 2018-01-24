c9a7bbaae14e1a5db0b649e0b04f83ff56c01bc3
bgklein7 <bgklein@outlook.com>
2018-01-24 15:32:49 -0500

    second loops

--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -75,34 +75,34 @@ namespace PracticeProblems
             //valueTwo = 5.12;
             //Console.WriteLine(valueOne + valueTwo);
 
-            int[] numLPs = new int[25];
+            //int[] numLPs = new int[25];
 
-            string[] genreLPs = new string[25];
-            genreLPs[0] = "Rock";
-            genreLPs[1] = "Alternative Rock";
-            genreLPs[2] = "Indie Rock";
-            genreLPs[3] = "Blues Rock";
-            genreLPs[4] = "Oldies";
-            genreLPs[5] = "Rockabilly";
-            genreLPs[6] = "Psychobilly";
-            genreLPs[7] = "Bluegrass";
-            genreLPs[8] = "Metal";
-            genreLPs[9] = "Soul";
-            genreLPs[10] = "Folk";
-            genreLPs[11] = "Bebop";
-            genreLPs[12] = "Swing";
-            genreLPs[13] = "Hip Hop";
-            genreLPs[14] = "Rap";
-            genreLPs[15] = "Classical";
-            genreLPs[16] = "Smooth Jazz";
-            genreLPs[17] = "Alt Country";
-            genreLPs[18] = "Roots Rock";
-            genreLPs[19] = "Art House";
-            genreLPs[20] = "House";
-            genreLPs[21] = "Electro";
-            genreLPs[22] = "Drum and Bass";
-            genreLPs[23] = "Soundtracks";
-            genreLPs[24] = "Christmas";
+            //string[] genreLPs = new string[25];
+            //genreLPs[0] = "Rock";
+            //genreLPs[1] = "Alternative Rock";
+            //genreLPs[2] = "Indie Rock";
+            //genreLPs[3] = "Blues Rock";
+            //genreLPs[4] = "Oldies";
+            //genreLPs[5] = "Rockabilly";
+            //genreLPs[6] = "Psychobilly";
+            //genreLPs[7] = "Bluegrass";
+            //genreLPs[8] = "Metal";
+            //genreLPs[9] = "Soul";
+            //genreLPs[10] = "Folk";
+            //genreLPs[11] = "Bebop";
+            //genreLPs[12] = "Swing";
+            //genreLPs[13] = "Hip Hop";
+            //genreLPs[14] = "Rap";
+            //genreLPs[15] = "Classical";
+            //genreLPs[16] = "Smooth Jazz";
+            //genreLPs[17] = "Alt Country";
+            //genreLPs[18] = "Roots Rock";
+            //genreLPs[19] = "Art House";
+            //genreLPs[20] = "House";
+            //genreLPs[21] = "Electro";
+            //genreLPs[22] = "Drum and Bass";
+            //genreLPs[23] = "Soundtracks";
+            //genreLPs[24] = "Christmas";
 
             //for (int i = 0; i < numLPs.Length; i++)
             //{
@@ -110,31 +110,31 @@ namespace PracticeProblems
             //    numLPs[i] = int.Parse(Console.ReadLine());
             //}
 
-            numLPs[0] = 123;
-            numLPs[1] = 256;
-            numLPs[2] = 237;
-            numLPs[3] = 65;
-            numLPs[4] = 24;
-            numLPs[5] = 143;
-            numLPs[6] = 237;
-            numLPs[7] = 24;
-            numLPs[8] = 54;
-            numLPs[9] = 234;
-            numLPs[10] = 12;
-            numLPs[11] = 209;
-            numLPs[12] = 32;
-            numLPs[13] = 67;
-            numLPs[14] = 45;
-            numLPs[15] = 165;
-            numLPs[16] = 39;
-            numLPs[17] = 78;
-            numLPs[18] = 230;
-            numLPs[19] = 32;
-            numLPs[20] = 345;
-            numLPs[21] = 31;
-            numLPs[22] = 89;
-            numLPs[23] = 101;
-            numLPs[24] = 25;
+            ////numLPs[0] = 123;
+            ////numLPs[1] = 256;
+            ////numLPs[2] = 237;
+            ////numLPs[3] = 65;
+            ////numLPs[4] = 24;
+            ////numLPs[5] = 143;
+            ////numLPs[6] = 237;
+            ////numLPs[7] = 24;
+            ////numLPs[8] = 54;
+            ////numLPs[9] = 234;
+            ////numLPs[10] = 12;
+            ////numLPs[11] = 209;
+            ////numLPs[12] = 32;
+            ////numLPs[13] = 67;
+            ////numLPs[14] = 45;
+            ////numLPs[15] = 165;
+            ////numLPs[16] = 39;
+            ////numLPs[17] = 78;
+            ////numLPs[18] = 230;
+            ////numLPs[19] = 32;
+            ////numLPs[20] = 345;
+            ////numLPs[21] = 31;
+            ////numLPs[22] = 89;
+            ////numLPs[23] = 101;
+            ////numLPs[24] = 25;
 
             //Console.WriteLine("Vinyl Inventory");
 
@@ -143,14 +143,40 @@ namespace PracticeProblems
             //    Console.WriteLine(genreLPs[i] + ": " + numLPs[i]);
             //}
 
-            for (int i = 0; i < numLPs.Length; i++)
+            //for (int i = 0; i < numLPs.Length; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        Console.WriteLine(numLPs[i]);
+            //    }
+            //}
+
+            int[] userNum = new int[10];
+
+            int sum = 0; //for second solution to problem, ie not needed in first solution
+
+            for (int i = 0; i < userNum.Length; i++)
             {
-                if (i % 2 == 1)
-                    {
-                    Console.WriteLine(numLPs[i]);
-                }
+                Console.WriteLine("Please enter a whole number " + i);
+                userNum[i] = int.Parse(Console.ReadLine());
+
+            
+            //int sum = userNum[1] + userNum[2]+ userNum[3]+ userNum[4] + userNum[5] + userNum[6] + userNum[7] + userNum[8] + userNum[9] + userNum[10];
+            sum += userNum[i];
+            }
+            Console.WriteLine(sum);
+
+            if (sum % 2 == 0)
+            {
+                Console.WriteLine("even");
             }
 
+            else
+            {
+                Console.WriteLine("odd");
+            }
+
+            
         }
     }
 }
